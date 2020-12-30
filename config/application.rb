3325649fb6d0dd2ca91df49fb8842ad775e3215f
Le Duc Son <le.duc.son@sun-asterisk.com>
2020-12-30 10:21:46 +0700

    edit

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,12 @@ module Training
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.autoload_paths += %W(#{config.root}/config/routes)
+
+    config.generators.system_tests = nil
+    config.generators.helper = false
+    config.generators.skip_routes = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
