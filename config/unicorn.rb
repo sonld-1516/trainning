bc6531e94618b0ba6396132ed49610ed95ba647a
Le Duc Son <le.duc.son@sun-asterisk.com>
2020-12-31 17:18:26 +0700

    c

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,11 +9,11 @@ preload_app true
 timeout 30
 
 # Path for the Unicorn socket
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_dir}/tmp/sockets/unicorn.sock", :backlog => 64
 
 # Set path for logging
 stderr_path "#{shared_dir}/log/unicorn.stderr.log"
 stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set proccess id path
-pid "#{shared_dir}/pids/unicorn.pid"
+pid "#{shared_dir}/tmp/pids/unicorn.pid"
